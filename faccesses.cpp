8fde371cc153bfdc3c8b480fa59e11710700b03f
Sana (Q) <@CocoaPuff2>
2025-12-06 19:57:52 -0800

    testing

--- faccesses.cpp
+++ faccesses.cpp
@@ -28,7 +28,15 @@ int filewrite( const char* filename, int nblocks, char* block ) {
 
   // repetitively write n times, (nblocks) of block (4096B)
   // writes nblocks that are 4096 each
+  // fd: int, returned by open, uniquely identifies the open file in the OS.
+  //     used in write(), read(), and close() to refer to that file.
+
+  // block: pointer to a memory buffer (char*) of size FILEBLOCK that holds the data to
+  //         write to the file (or where data read into)
+
+  // FILEBLOCK:  macro defined as 4096, representing the size of one file block in bytes (4 KB)
   for (int i = 0; i < nblocks; i++) {
+      // Attempt to write FILEBLOCK (4096) bytes from the buffer to the file
       ssize_t bytes_written = write(fd, block, FILEBLOCK);
       if (bytes_written < 0) {
           cerr << "Writing error." << endl;
@@ -56,7 +64,7 @@ int fileread( const char* filename, int nblocks, char* block ) {
   // open a file for reading.
   int fd = open(filename, O_RDONLY);
   if (fd < 0) {
-      cerr << "Read error" << endl;
+      cerr << "Read error, can't open file " << endl;
       return -1;
   }
 
@@ -66,11 +74,15 @@ int fileread( const char* filename, int nblocks, char* block ) {
   gettimeofday(&start_time, NULL);
 
   // repetitively read n times, (nblocks) of block (4096B)
+  // reads nblocks of size 4096
   for (int i = 0; i < nblocks; i++) {
-      // Try to read FILEBLOCK bytes (4096) into the buffer block
-      // read() returns  number of bytes read (or -1)
-      if (read (fd, block, FILEBLOCK) < 0) {
+      ssize_t bytes_read = read(fd, block, FILEBLOCK);
+      if (bytes_read < 0) {
           cerr << "Reading error" << endl;
+          close(fd);
+          return -1;
+      } else if (bytes_read < FILEBLOCK) {
+          cerr << "Partial read occured" << endl;
           close(fd);
           return -1;
       }
@@ -78,6 +90,7 @@ int fileread( const char* filename, int nblocks, char* block ) {
 
   // stop timer
   gettimeofday( &end_time, NULL );
+
   time_t elapsed_time = ( end_time.tv_sec - start_time.tv_sec ) * 1000000 + ( end_time.tv_usec - start_time.tv_usec );
   cerr << "nblocks = " << nblocks << ", total time = " <<  elapsed_time << ", time / block = "<< elapsed_time / nblocks << endl;
 
@@ -93,42 +106,52 @@ string filename( int i ){
   return filename_str;
 }
 
-int main( int argc, char* argv[] ) {
+int main(int argc, char* argv[]) {
+    // initialize block with repeating letters
+    char block[FILEBLOCK];
+    for (int i = 0; i < FILEBLOCK; i++)
+        block[i] = 'a' + (i % NCHARS);
 
-  // initialize block
-  char block[FILEBLOCK];
-  for ( int i = 0; i < FILEBLOCK; i++ )
-    block[i] = 'a' + ( i % NCHARS );
+    cout << "writing to direct blocks ********************************************" << endl;
+    filewrite(filename(1).c_str(), 1, block);    // 1 block, first direct pointer
+    filewrite(filename(12).c_str(), 12, block);  // 12 blocks, all direct pointers
 
-  cout << "writing to direct blocks ********************************************" << endl;
-  filewrite( filename( 1 ).c_str(), 1, block );
-  filewrite( filename( 12 ).c_str(), 12, block );
+    cout << "writing to 1st indirect blocks **************************************" << endl;
+    int single_indirect_blocks = 256; // 4 KB / 4 Bytes per pointer = 1024 ptrs, so went with 256
+    filewrite(filename(13).c_str(), 13, block);              // direct + first of single indirect
+    filewrite(filename(12 + single_indirect_blocks).c_str(), 12 + single_indirect_blocks, block); // full single indirect
 
-  cout << "writing to 1st indirect blocks **************************************" << endl;
-  // filewrite all the way to the 1st block of the 1st indirect blocks
-  // filewrite all the way to the last block of the 1st indirect blocks
+    cout << "writing to 2ndary indirect blocks *******************************" << endl;
+    int double_indirect_blocks = 256 * 256;
+    filewrite(filename(12 + single_indirect_blocks + 1).c_str(), 12 + single_indirect_blocks + 1, block); // first block of double indirect
+    filewrite(filename(12 + single_indirect_blocks + double_indirect_blocks).c_str(),
+              12 + single_indirect_blocks + double_indirect_blocks, block); // full double indirect
 
-  cout << "writing to the 2ndary indirect blocks *******************************" << endl;
-  // filewrite all the way to the 1st block of the secandary indirect blocks
-  // filewrite all the way to the last block of the secandary indirect blocks
-  
-  cout << "writing to the 3tiary indirect blocks *******************************" << endl;
-  // filewrite all the way to the 1st block of the tartiary indirect blocks
+    // --- Triple indirect blocks ---
+    cout << "writing to 3tiary indirect blocks *******************************" << endl;
+    int triple_indirect_blocks = 256 * 256 * 256;
+    filewrite(filename(12 + single_indirect_blocks + double_indirect_blocks + 1).c_str(),
+              12 + single_indirect_blocks + double_indirect_blocks + 1, block);
 
-  cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
 
-  cout << "reading to direct blocks ********************************************" << endl;
-  fileread( filename( 1 ).c_str(), 1, block );
-  fileread( filename( 12 ).c_str(), 12, block );  
+    // --- Reading files ---
+    cout << "reading direct blocks ********************************************" << endl;
+    fileread(filename(1).c_str(), 1, block);
+    fileread(filename(12).c_str(), 12, block);
 
-  cout << "reading to 1st indirect blocks **************************************" << endl;
-  // filewrite all the way to the 1st block of the 1st indirect blocks
-  // filewrite all the way to the last block of the 1st indirect blocks
+    cout << "reading 1st indirect blocks **************************************" << endl;
+    fileread(filename(13).c_str(), 13, block);
+    fileread(filename(12 + single_indirect_blocks).c_str(), 12 + single_indirect_blocks, block);
 
-  cout << "reading to the 2ndary indirect blocks *******************************" << endl;
-  // filewrite all the way to the 1st block of the secandary indirect blocks
-  // filewrite all the way to the last block of the secandary indirect blocks
+    cout << "reading 2ndary indirect blocks *******************************" << endl;
+    fileread(filename(12 + single_indirect_blocks + 1).c_str(), 12 + single_indirect_blocks + 1, block);
+    fileread(filename(12 + single_indirect_blocks + double_indirect_blocks).c_str(),
+             12 + single_indirect_blocks + double_indirect_blocks, block);
 
-  cout << "reading to the 3tiary indirect blocks *******************************" << endl;
-  // filewrite all the way to the 1st block of the tartiary indirect blocks
+    cout << "reading 3tiary indirect blocks *******************************" << endl;
+    fileread(filename(12 + single_indirect_blocks + double_indirect_blocks + 1).c_str(),
+             12 + single_indirect_blocks + double_indirect_blocks + 1, block);
+
+    return 0;
 }
