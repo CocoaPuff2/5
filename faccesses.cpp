f87d482fd641daf8a0211f7bdc7c8a308952afc8
Sana (Q) <@CocoaPuff2>
2025-12-06 19:40:07 -0800

    working...

---
+++ faccesses.cpp
@@ -0,0 +1,134 @@
+#include <fcntl.h>    // open
+#include <unistd.h>   // write, close
+#include <iostream>   // cerr
+#include <stdlib.h>   // atoi
+#include <sys/time.h> // gettimeofday
+
+#define NCHARS    26
+#define FILEBLOCK 4096
+
+using namespace std;
+
+int filewrite( const char* filename, int nblocks, char* block ) {
+  // open a file for writing.
+  // WRONLY: opens file, write access only
+  // CREAT: create file is not already exists
+  // TRUNC: if file exists, truncate to 0 bytes, erases all content
+  // 0666 is permission bits for writing a new file
+  int fd = open(filename, O_WRONLY, O_CREAT, O_TRUNC, 0666);
+  if (fd < 0) {
+      cerr << "Error opening file to write" << endl;
+      return -1;
+  }
+
+  // start timer
+  struct timeval start_time, end_time;
+  // record current time before writing begins
+  gettimeofday(&start_time, NULL);
+
+  // repetitively write n times, (nblocks) of block (4096B)
+  // writes nblocks that are 4096 each
+  for (int i = 0; i < nblocks; i++) {
+      ssize_t bytes_written = write(fd, block, FILEBLOCK);
+      if (bytes_written < 0) {
+          cerr << "Writing error." << endl;
+          close(fd);
+          return -1;
+      } else if (bytes_written < FILEBLOCK) {
+          cerr << "Partial write occurred." << endl;
+          close(fd);
+          return -1;
+      }
+  }
+
+  close(fd); // flush all in-cache data to disk
+
+  // stop timer
+  gettimeofday( &end_time, NULL );
+
+  time_t elapsed_time = ( end_time.tv_sec - start_time.tv_sec ) * 1000000 + ( end_time.tv_usec - start_time.tv_usec );
+  cerr << "nblocks = " << nblocks << ", total time = " <<  elapsed_time << ", time / block = "<< elapsed_time / nblocks << endl;
+  
+  return 0;
+}
+
+int fileread( const char* filename, int nblocks, char* block ) {
+  // open a file for reading.
+  int fd = open(filename, O_RDONLY);
+  if (fd < 0) {
+      cerr << "Read error" << endl;
+      return -1;
+  }
+
+  // start timer
+  struct timeval start_time, end_time;
+  // get time before reading
+  gettimeofday(&start_time, NULL);
+
+  // repetitively read n times, (nblocks) of block (4096B)
+  for (int i = 0; i < nblocks; i++) {
+      // Try to read FILEBLOCK bytes (4096) into the buffer block
+      // read() returns  number of bytes read (or -1)
+      if (read (fd, block, FILEBLOCK) < 0) {
+          cerr << "Reading error" << endl;
+          close(fd);
+          return -1;
+      }
+  }
+
+  // stop timer
+  gettimeofday( &end_time, NULL );
+  time_t elapsed_time = ( end_time.tv_sec - start_time.tv_sec ) * 1000000 + ( end_time.tv_usec - start_time.tv_usec );
+  cerr << "nblocks = " << nblocks << ", total time = " <<  elapsed_time << ", time / block = "<< elapsed_time / nblocks << endl;
+
+  close(fd); // read closes after the timer stops as there are no data remained in cache.
+  
+  return 0;
+}
+
+// returns the name of a file that includes i blocks.
+string filename( int i ){
+  string num = to_string( i );
+  string filename_str = "f_" + num + ".txt";
+  return filename_str;
+}
+
+int main( int argc, char* argv[] ) {
+
+  // initialize block
+  char block[FILEBLOCK];
+  for ( int i = 0; i < FILEBLOCK; i++ )
+    block[i] = 'a' + ( i % NCHARS );
+
+  cout << "writing to direct blocks ********************************************" << endl;
+  filewrite( filename( 1 ).c_str(), 1, block );
+  filewrite( filename( 12 ).c_str(), 12, block );
+
+  cout << "writing to 1st indirect blocks **************************************" << endl;
+  // filewrite all the way to the 1st block of the 1st indirect blocks
+  // filewrite all the way to the last block of the 1st indirect blocks
+
+  cout << "writing to the 2ndary indirect blocks *******************************" << endl;
+  // filewrite all the way to the 1st block of the secandary indirect blocks
+  // filewrite all the way to the last block of the secandary indirect blocks
+  
+  cout << "writing to the 3tiary indirect blocks *******************************" << endl;
+  // filewrite all the way to the 1st block of the tartiary indirect blocks
+
+  cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+
+  cout << "reading to direct blocks ********************************************" << endl;
+  fileread( filename( 1 ).c_str(), 1, block );
+  fileread( filename( 12 ).c_str(), 12, block );  
+
+  cout << "reading to 1st indirect blocks **************************************" << endl;
+  // filewrite all the way to the 1st block of the 1st indirect blocks
+  // filewrite all the way to the last block of the 1st indirect blocks
+
+  cout << "reading to the 2ndary indirect blocks *******************************" << endl;
+  // filewrite all the way to the 1st block of the secandary indirect blocks
+  // filewrite all the way to the last block of the secandary indirect blocks
+
+  cout << "reading to the 3tiary indirect blocks *******************************" << endl;
+  // filewrite all the way to the 1st block of the tartiary indirect blocks
+}
